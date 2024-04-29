b561d0fd2d2a0028ca0de78a70061ca91f0e8961
Cristhian2430 <82115251+Cristhian2430@users.noreply.github.com>
2024-04-29 18:35:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 import streamlit as st
 import pandas as pd
+import datetime
 
 st.set_page_config(page_title   = "RSF", 
-                   page_icon    = ":syringe:",
+                   page_icon    = ":chart:",
                    layout       = "wide")
 
 st.title('Reserva Secundaria de Frecuencia (RSF)')
 
-ruta_trabajo = st.text_input('Ingrese la ruta de trabajo donde desea guardar los archivos:', '')
+ruta_trabajo = st.text_input('Ruta de Trabajo:', '')
 
+today = datetime.datetime.now()
+next_year = today.year + 1
+jan_1 = datetime.date(next_year, 1, 1)
+dec_31 = datetime.date(next_year, 12, 31)
+
+d = st.date_input(
+    "Select your vacation for next year",
+    (jan_1, datetime.date(next_year, 1, 7)),
+    jan_1,
+    dec_31,
+    format="MM.DD.YYYY",
+)
+d
 
 # Mostrar el texto procesado
 st.write('Texto sin espacios:', processed_text)
